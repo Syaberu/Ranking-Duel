fa3d441c734c6b2446deb3d2cd8a69e202f3a6b1
Syaberu <syaberu9@gmail.com>
2026-06-21 14:37:54 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+// data.js
+window.players = [
+    { 
+        name: "MCsyaberu", 
+        mainKit: "Sword", 
+        beatenByKit: {
+            sword: [], 
+            axe: [],          
+            bow: [],                 
+            uhc: []           
+        }
+    },
+   
+];
