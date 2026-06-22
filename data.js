f20a9e13b93c949bec7f62e3ac57e8d9b6273376
Syaberu <syaberu9@gmail.com>
2026-06-23 00:56:33 +0900

    Add new player and update match scores
    Added new player 'Ahirusan4414' with match scores.

--- data.js
+++ data.js
@@ -6,7 +6,8 @@ window.players = [
         matchScores: {
             MCsyaberu:    { sword: [3, 1], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
@@ -15,7 +16,8 @@ window.players = [
         matchScores: {
             AntiUnlockJP: { sword: [1, 3], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             biribirijin:  { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Ahirusan4414: { sword: [2, 2], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 2-2 引き分け
         }
     },
     {
@@ -24,16 +26,28 @@ window.players = [
         matchScores: {
             AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             MCsyaberu:    { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Reiteirei:    { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 4-0 で勝利
+            Reiteirei:    { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
         name: "Reiteirei",
-        mainKit: "Sword", // 仮のメインキットとしてSwordを設定
+        mainKit: "Sword",
         matchScores: {
             AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             MCsyaberu:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            biribirijin:  { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 0-4 で敗北
+            biribirijin:  { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+        }
+    },
+    {
+        name: "Ahirusan4414",
+        mainKit: "Sword", // 仮のメインキット
+        matchScores: {
+            AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            MCsyaberu:    { sword: [2, 2], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }, // 2-2 引き分け
+            biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     }
 ];
