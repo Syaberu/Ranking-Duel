37fb6423f11568a9d268f6c0e6612a834ba03911
Syaberu <syaberu9@gmail.com>
2026-06-22 21:44:57 +0900

    Update data.js

--- data.js
+++ data.js
@@ -4,9 +4,9 @@ window.players = [
         name: "AntiUnlockJP",
         mainKit: "Sword",
         matchScores: {
-            // 戦っていないキット（axe, bow, uhc）はすべて [0, 0] に設定
             MCsyaberu:    { sword: [3, 1], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
@@ -14,7 +14,8 @@ window.players = [
         mainKit: "Sword",
         matchScores: {
             AntiUnlockJP: { sword: [1, 3], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            biribirijin:  { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            biribirijin:  { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
@@ -22,7 +23,17 @@ window.players = [
         mainKit: "Sword",
         matchScores: {
             AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            MCsyaberu:    { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            MCsyaberu:    { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Reiteirei:    { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 4-0 で勝利
+        }
+    },
+    {
+        name: "Reiteirei",
+        mainKit: "Sword", // 仮のメインキットとしてSwordを設定
+        matchScores: {
+            AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            MCsyaberu:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            biribirijin:  { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 0-4 で敗北
         }
     }
 ];
