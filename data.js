5be2cccadafc2b2d625fc75945ce669120607b04
Syaberu <syaberu9@gmail.com>
2026-06-22 21:32:50 +0900

    Update data.js

--- data.js
+++ data.js
@@ -2,18 +2,27 @@
 window.players = [
     {
         name: "AntiUnlockJP",
-        mainKit: "Sword", // 任意のメインキット
+        mainKit: "Sword",
         matchScores: {
-            // AntiUnlockJPはMCsyaberuに3勝1敗 [自分の勝ち, 相手の勝ち]
-            MCsyaberu: { sword: [3, 1], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            // 戦っていないキット（axe, bow, uhc）はすべて [0, 0] に設定
+            MCsyaberu:    { sword: [3, 1], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
         name: "MCsyaberu",
         mainKit: "Sword",
         matchScores: {
-            // MCsyaberuはAntiUnlockJPに1勝3敗 [自分の勝ち, 相手の勝ち]
-            AntiUnlockJP: { sword: [1, 3], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            AntiUnlockJP: { sword: [1, 3], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            biribirijin:  { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+        }
+    },
+    {
+        name: "biribirijin",
+        mainKit: "Sword",
+        matchScores: {
+            AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            MCsyaberu:    { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     }
 ];
