896752341a401efa097f5c0daeb2af0b42a2607e
Syaberu <syaberu9@gmail.com>
2026-06-22 20:21:33 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,14 +1,19 @@
 // data.js
 window.players = [
-    { 
-        name: "MCsyaberu", 
-        mainKit: "Sword", 
-        beatenByKit: {
-            sword: [], 
-            axe: [],          
-            bow: [],                 
-            uhc: []           
+    {
+        name: "AntiUnlockJP",
+        mainKit: "Sword", // 任意のメインキット
+        matchScores: {
+            // AntiUnlockJPはMCsyaberuに3勝1敗 [自分の勝ち, 相手の勝ち]
+            MCsyaberu: { sword: [3, 1], axe: [3, 1], bow: [3, 1], uhc: [3, 1] }
         }
     },
-   
+    {
+        name: "MCsyaberu",
+        mainKit: "Sword",
+        matchScores: {
+            // MCsyaberuはAntiUnlockJPに1勝3敗 [自分の勝ち, 相手の勝ち]
+            AntiUnlockJP: { sword: [1, 3], axe: [1, 3], bow: [1, 3], uhc: [1, 3] }
+        }
+    }
 ];
