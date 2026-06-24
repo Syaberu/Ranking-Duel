ba8e129ec0c393371b6c80e078f39b66782afe78
Syaberu <syaberu9@gmail.com>
2026-06-24 20:36:09 +0900

    Update data.js

--- data.js
+++ data.js
@@ -7,7 +7,8 @@ window.players = [
             MCsyaberu:    { sword: [3, 1], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            dracaena08:   { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
@@ -17,7 +18,8 @@ window.players = [
             AntiUnlockJP: { sword: [1, 3], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             biribirijin:  { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Ahirusan4414: { sword: [2, 2], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 2-2 引き分け
+            Ahirusan4414: { sword: [2, 2], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            dracaena08:   { sword: [1, 3], axe: [0, 0], bow: [0, 0], uhc: [0, 0] } // 1-3 で敗北
         }
     },
     {
@@ -27,7 +29,8 @@ window.players = [
             AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             MCsyaberu:    { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             Reiteirei:    { sword: [4, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            dracaena08:   { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
@@ -37,17 +40,30 @@ window.players = [
             AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             MCsyaberu:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             biribirijin:  { sword: [0, 4], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            dracaena08:   { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     },
     {
         name: "Ahirusan4414",
-        mainKit: "Sword", // 仮のメインキット
+        mainKit: "Sword",
         matchScores: {
             AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            MCsyaberu:    { sword: [2, 2], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }, // 2-2 引き分け
+            MCsyaberu:    { sword: [2, 2], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
             biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
-            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            dracaena08:   { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
+        }
+    },
+    {
+        name: "dracaena08",
+        mainKit: "Sword",
+        matchScores: {
+            AntiUnlockJP: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            MCsyaberu:    { sword: [3, 1], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }, // 3-1 で勝利
+            biribirijin:  { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Reiteirei:    { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] },
+            Ahirusan4414: { sword: [0, 0], axe: [0, 0], bow: [0, 0], uhc: [0, 0] }
         }
     }
 ];
