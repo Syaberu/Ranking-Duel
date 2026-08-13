b7fd5b1df347507802fda259e05d0fdcabf78f5e
Syaberu <syaberu9@gmail.com>
2026-08-14 07:27:29 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,13 +1,3 @@
-// ==========================================
-// 対戦履歴データ（新規結果は一番下に追記）
-// 
-// 【書き方】
-// date: "日付"
-// kit: "Sword" | "Axe" | "Bow" | "UHC"
-// p1: プレイヤー1, s1: p1の取得セット数, r1: 勝敗後の順位(#)
-// p2: プレイヤー2, s2: p2の取得セット数, r2: 勝敗後の順位(#)
-// ==========================================
-
 const rawMatchLogs = [
   { date: "2026/08/01 17:48", kit: "Sword", p1: "MCsyaberu", s1: 3, r1: 1, p2: "dadan11pvp", s2: 1, r2: 2 },
   { date: "2026/08/01 18:12", kit: "UHC",   p1: "MCsyaberu", s1: 3, r1: 1, p2: "UnsaidLand4664", s2: 0, r2: 2 },
@@ -31,4 +21,5 @@ const rawMatchLogs = [
   { date: "2026/08/09 18:55", kit: "UHC",   p1: "MCsyaberu", s1: 3, r1: 1, p2: "UnsaidLand4664", s2: 1, r2: 2 },
   { date: "2026/08/09 19:17", kit: "Sword", p1: "Shokai3197050", s1: 0, r1: 2, p2: "AntiUnlockJP", s2: 0, r2: 3 },
   { date: "2026/08/10 18:49", kit: "Sword", p1: "Reiteirei", s1: 3, r1: 7, p2: "VoteableArt9345", s2: 1, r2: 8 },
+  { date: "2026/08/14 00:58", kit: "Mace",  p1: "MCsyaberu", s1: 3, r1: 1, p2: "Reiteirei", s2: 0, r2: 2 },
 ];
